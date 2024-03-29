d9b4c2ca0be1ae399ae2141cf5e1b12a2439f019
Pham Kim Hoan <pham0030@gmail.com>
2024-03-29 20:38:04 +0700

    fix script1 and 2

--- script1.py
+++ script1.py
@@ -1,2 +1,5 @@
 def function1():
     print('this is first python script')
+
+    with open('function1_output.txt', 'a') as of:
+        of.write('this is first python script' + '\n')
--- script2.py
+++ script2.py
@@ -1,2 +1,5 @@
 def function2():
     print('this is seconde python script')
+
+    with open('function2_output.txt', 'a') as of:
+        of.write('this is second python script' + '\n')
