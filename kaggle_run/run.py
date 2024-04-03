ba4138f4bff1dea7744bffa7e26337b278fa79af
Pham Kim Hoan <pham0030@gmail.com>
2024-04-03 16:31:52 +0700

    add kaggle script for running

---
+++ kaggle_run/run.py
@@ -0,0 +1,3 @@
+import subprocess
+subprocess.run(["git", "clone", "https://github.com/pham0030/kaggle_demo_script_on_github"])
+subprocess.run(["python", "kaggle_demo_script_on_github/bigram_demo.py"])
